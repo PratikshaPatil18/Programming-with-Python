d5e8358404eecd19f66859789cb9507b0d017054
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-10 23:44:40 +0530

    Scope of Variable
    commit here

---
+++ Variable.py
@@ -0,0 +1,21 @@
+#Variable
+
+#declaration and assign value to variable.
+
+roll_no=1;
+print('Roll No :',roll_no);
+
+#declaration and assign same value to different variable.
+
+a=b=c=101;
+print('value of a :',a);
+print('value of b :',b);
+print('value of c :',c);
+
+#declaration and assign different value to different variable.
+
+x,y,z='10.23','12','Patil';
+print('value of x :',x);
+print('value of y :',y);
+print('value of z :',z);
+
