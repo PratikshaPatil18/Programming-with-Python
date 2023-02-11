bf3b51f025ef7d84a4964303d8bb291bc6275e27
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-11 23:34:42 +0530

    Python Dictionary
    commit here

---
+++ Dictonary.py
@@ -0,0 +1,9 @@
+#python Dictonary
+a={'a':'tasgaon','b':'chinchani','c':'sangli'};
+print('value of a :',a);
+print('datatype of a :',type(a));
+print('1st city :',a['a']);
+print("all keys :",a.keys());
+print("all values :",a.values());
+      
+
