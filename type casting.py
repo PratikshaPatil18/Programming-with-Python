550ca6b48456aecf59b8b42a2593cdb23944b287
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-11 11:52:52 +0530

    Type Casting Using Python
    commit here

---
+++ type casting.py
@@ -0,0 +1,18 @@
+#implicit type casting.
+
+a=10;
+b=1.23;
+c=a+b;
+print("datatype of a :",type(a));
+print("datatype of b :",type(b));
+print("datatype of c :",type(c));
+print("/n");
+
+#Explicit type casting.
+
+a=10;
+b=1.23;
+c=(float)(a+b);
+print("datatype of a :",type(a));
+print("datatype of b :",type(b));
+print("datatype of c :",type(c));
