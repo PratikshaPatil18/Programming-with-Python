b0775353a4f996241a60fe5f93e72a5120b4d63a
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-12 18:52:39 +0530

    break and continue statements
    commit here

---
+++ break and continue statement.py
@@ -0,0 +1,15 @@
+#break statement.
+i=1;
+while(i<=5):
+    print(i);
+    if(i==3):
+        break
+    i=i+1;
+
+#continue statement.
+num=1;
+while(num<=5):
+    print(num);
+    if(num==3):
+        continue
+    i=i+1;
