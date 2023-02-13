2542a8a0720b1bce29604c960b20c403ed3693e8
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2023-02-13 07:51:05 +0530

    use of range function.
    commit here

---
+++ range function.py
@@ -0,0 +1,12 @@
+#range()function.
+a= list(range(10))
+print("list=",a);
+
+b=set(range(5))
+print("set =",b);
+
+c=tuple(range(10,20,2))
+print("tuple =",c);
+
+d=list(range(20,1,-2))
+print("list =",d);
